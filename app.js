4bc373403e75ec0dee153b85ea57844cec318474
maxirivero <maximilianorivero999@gmail.com>
2021-05-12 15:55:30 -0300

    Incremento de Switch

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 const {sumar,restar,multiplicar,dividir} = require('./calculadora')
+
+switch (process.argv[2]){
+    case "sumar":
+        sumar(process.argv[3],process.argv[4]);
+        break;
+    case "restar":
+        restar(process.argv[3],process.argv[4]);
+        break;
+
+    default:
+        break;
+}
